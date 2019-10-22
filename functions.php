5f61fc3e5d39849faadb36c7c034438197e8deea
yllipetrovci <yllipetrovci@gmail.com>
2019-10-23 01:27:36 +0200

    change stylesheet load file, add vue template compailer in package.json

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 function load_stylesheets()
 {
 
-    wp_register_style('stylesheet', get_template_directory_uri() . '/style.css', '', 1, 'all');
+    wp_register_style('stylesheet', get_template_directory_uri() . '/app.css', '', 1, 'all');
     wp_enqueue_style('stylesheet');
 }
 add_action('wp_enqueue_scripts', 'load_stylesheets');
