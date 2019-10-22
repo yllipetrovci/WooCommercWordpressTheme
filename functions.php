0a16945dbd77d6859efe28b7693dcb6dfa7e0aa5
yllipetrovci <yllipetrovci@gmail.com>
2019-10-23 01:04:52 +0200

    add javascript function

--- functions.php
+++ functions.php
@@ -6,5 +6,11 @@ function load_stylesheets()
     wp_register_style('stylesheet', get_template_directory_uri() . '/style.css', '', 1, 'all');
     wp_enqueue_style('stylesheet');
 }
+add_action('wp_enqueue_scripts', 'load_stylesheets');
 
-add_action('wp_enqueue_scripts','load_stylesheets');
+function load_javascript()
+{
+    wp_register_script('custom', get_template_directory_uri() . '/app.js', 'jquery', 1, true);
+    wp_enqueue_script('custom');
+}
+add_action('wp_enqueue_scripts', 'load_javascript');
