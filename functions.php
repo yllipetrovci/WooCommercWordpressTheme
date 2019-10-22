72074999ebb1093911ecdb4f810fdf8268025664
yllipetrovci <yllipetrovci@gmail.com>
2019-10-23 01:51:05 +0200

    add support for menu, add a menu location

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ * Add stylesheet
+ */
 function load_stylesheets()
 {
 
@@ -8,9 +10,28 @@ function load_stylesheets()
 }
 add_action('wp_enqueue_scripts', 'load_stylesheets');
 
+/**
+ * Add javascript
+ */
 function load_javascript()
 {
     wp_register_script('custom', get_template_directory_uri() . '/app.js', 'jquery', 1, true);
     wp_enqueue_script('custom');
 }
 add_action('wp_enqueue_scripts', 'load_javascript');
+
+
+/**
+ * Add menus
+ */
+
+ add_theme_support('menus');
+
+/**
+ * Register Menus
+ */
+register_nav_menus(
+array(
+    'top-menu'=> 'Top Menu'
+)
+);
