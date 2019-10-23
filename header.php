2cb8d5905dac96690e5b53a4700939ce0562fc68
yllipetrovci <yllipetrovci@gmail.com>
2019-10-23 02:01:46 +0200

    add top menu, add styling for top menu

--- header.php
+++ header.php
@@ -6,11 +6,19 @@
     <?php wp_head(); ?>
 </head>
 
-<body <?php body_class();?>>
+<body <?php body_class(); ?>>
 
-<header>
-    <div class="container">
+    <header>
+        <div class="container d-flex align-items-center justify-content-between">
 
-    <img src="<?php bloginfo('template_directory');?>/images/logo.png" class="img-fluid logo"/>
-    </div>
-</header>
+            <img src="<?php bloginfo('template_directory'); ?>/images/logo.png" class="img-fluid logo" />
+            <?php
+            wp_nav_menu(
+                array(
+                    'theme_location'=>'top-menu',
+                    'menu_class'=>'top-menu d-flex '
+                )
+            );
+            ?>
+        </div>
+    </header>
